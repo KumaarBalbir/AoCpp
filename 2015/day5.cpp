c862f5f1bd4bd5e2e9c87e598aa0ecfb6037b19e
balbir <prasadbalbir1056@gmail.com>
2025-03-30 19:21:18 +0530

    2015: day5 part I

---
+++ 2015/day5.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <unordered_map>
+#include <algorithm>
+#include <cctype> // for isalpha
+using namespace std;
+
+unordered_map<std::string, int> exclMap;
+
+std::unordered_map<int,int>vowels;
+
+
+
+bool isNice(string str)
+{
+  
+  int n = str.length();
+  string sstr;
+  for(int i=0; i<n-1;i++)
+    {
+      sstr = str.substr(i,2);
+      
+
+	if(exclMap.find(sstr)!=exclMap.end())
+	  {
+	    return false;
+	  }
+    }
+
+  int cntVowel = 0;
+  for(int i=0;i<n;i++)
+    {
+      if(vowels[int(str[i]-'a')] == 1)
+	{
+	  cntVowel++;
+	}
+    }
+  if(cntVowel < 3)
+    {
+      return false;
+    }
+
+  bool ok = false;
+  for(int i=0;i<n-1;i++)
+    {
+      if(str[i] == str[i+1])
+	{
+	  ok = true;
+	  break;
+	}
+    }
+  
+  return ok;
+}
+
+bool alphaString(string str)
+{
+  for(char &ch:str)
+    {
+      if(!isalpha(ch))
+	{
+	  return false;
+	}
+    }
+  return true;
+}
+   
+  
+      
+int main()
+{
+exclMap["ab"] = 1;
+exclMap["cd"] = 1;
+exclMap["pq"] = 1;
+exclMap["xy"] = 1;
+
+vowels[0] = 1; // a
+vowels[4] = 1; // e
+vowels[8] = 1; // i
+vowels[14] = 1; // o
+vowels[20] = 1; // u
+
+ printf("%d", int('e'-'a'));
+
+  string fn = "day5-ip.txt";
+  ifstream ifs(fn);
+
+  if(!ifs.is_open())
+    {
+      printf("err opening ip file\n");
+      return 0;
+    }
+  int cnt = 0;
+  string curline;
+  int hell = 0;
+  while(getline(ifs, curline))
+    {
+      //  cout<<"debug: "<<curline<<"\n";
+      // hell++;
+      // if(hell == 5)
+      // 	{
+      // 	  break;
+      // 	}
+      // transform(curline.begin(), curline.end(), curline.begin(), ::tolower);
+      if(isNice(curline))
+	{
+	  cnt++;
+	}
+      // if(!alphaString(curline))
+      // 	{
+      // 	  cout<<"Hurray\n";
+      // 	  break;
+      // 	}
+    }
+  ifs.close();
+  cout<<"ans: "<<cnt<<"\n";
+  return 0;
+}
+      
+       
+   
