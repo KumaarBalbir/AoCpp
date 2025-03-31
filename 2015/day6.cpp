3d5d7f03dfa99865d75607c63bc347b861dbabdc
balbir <prasadbalbir1056@gmail.com>
2025-03-31 22:12:39 +0530

    2015: day6 part I

---
+++ 2015/day6.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+void getCoord(string str, int *res)
+{
+  int n = str.size();
+
+  int commandType = -1; // turn on = 1, turn off = 2, toggle = 3
+  string starting = str.substr(0, 7);
+  if (starting == "turn on")
+  {
+    commandType = 1;
+  }
+  else if (starting == "turn of")
+  {
+    commandType = 2;
+  }
+  else
+  {
+    commandType = 3;
+  }
+  int idx = 0;
+  string x1 = "";
+  string y1 = "";
+  string x2 = "";
+  string y2 = "";
+  for (idx = 0; idx < n; idx++)
+  {
+    if (isdigit(str[idx]))
+    {
+      x1 += str[idx];
+    }
+    if (str[idx] == ',')
+    {
+      idx++;
+      break;
+    }
+  }
+  for (; idx < n; idx++)
+  {
+    if (isdigit(str[idx]))
+    {
+      y1 += str[idx];
+    }
+    if (str[idx] == ' ')
+    {
+      idx++;
+      break;
+    }
+  }
+
+  for (; idx < n; idx++)
+  {
+    if (isdigit(str[idx]))
+    {
+      x2 += str[idx];
+    }
+    if (str[idx] == ',')
+    {
+      idx++;
+      break;
+    }
+  }
+  for (; idx < n; idx++)
+  {
+    if (isdigit(str[idx]))
+    {
+      y2 += str[idx];
+    }
+    if (str[idx] == ' ')
+    {
+      break;
+    }
+  }
+  res[0] = commandType;
+  res[1] = stoi(x1);
+  res[2] = stoi(y1);
+  res[3] = stoi(x2);
+  res[4] = stoi(y2);
+}
+
+int main()
+{
+  string fn = "day6-ip.txt";
+  ifstream ifs(fn);
+  if (!ifs.is_open())
+  {
+    cout << "error opening ip file\n";
+    return 0;
+  }
+
+  int W = 1000;
+  int H = 1000;
+  // initialize 2d grid W x H with false
+  vector<vector<bool>> grid(W, vector<bool>(H, false));
+  string curline;
+  int commandType, x1, y1, x2, y2;
+  int res[5];
+  while (getline(ifs, curline))
+  {
+    getCoord(curline, res);
+    commandType = res[0];
+    x1 = res[1];
+    y1 = res[2];
+    x2 = res[3];
+    y2 = res[4];
+    if (y1 > y2)
+    {
+      for (int i = x1; i <= x2; i++)
+      {
+        for (int j = y1; j >= y2; j--)
+        {
+          if (commandType == 1)
+          {
+            grid[i][j] = true;
+          }
+          else if (commandType == 2)
+          {
+            grid[i][j] = false;
+          }
+          else
+          {
+            grid[i][j] = !grid[i][j];
+          }
+        }
+      }
+    }
+    else
+    {
+      for (int i = x1; i <= x2; i++)
+      {
+        for (int j = y1; j <= y2; j++)
+        {
+          if (commandType == 1)
+          {
+            grid[i][j] = true;
+          }
+          else if (commandType == 2)
+          {
+            grid[i][j] = false;
+          }
+          else
+          {
+            grid[i][j] = !grid[i][j];
+          }
+        }
+      }
+    }
+  }
+  int totalOn = 0;
+  for (int i = 0; i < W; i++)
+    for (int j = 0; j < H; j++)
+    {
+      if (grid[i][j])
+      {
+        totalOn++;
+      }
+    }
+  cout << "ans: = " << totalOn << "\n";
+
+  return 0;
+}
